04f9411d718dbb2da39d0c4755cdec00faf17853
simone-2006 <simone.penza06@gmail.com>
2026-08-21 11:26:17 +0200

    Revert "Aggiunto il context e sistemato lo scorrimento con cursor."
    This reverts commit d19270a4b89b34514e39d01bc99c724329a8e855.

--- portfolio-2/src/App.jsx
+++ portfolio-2/src/App.jsx
@@ -1,4 +1,7 @@
 import Navbar from "./components/layout/Navbar";
+import Navbar, { navbarElements } from "./components/layout/Navbar";
+import { motion } from "framer-motion";
+
 import AboutMe from "./components/layout/AboutMe";
 import Skills from "./components/layout/Skills";
 import Experience from "./components/layout/Experience";
@@ -16,6 +19,31 @@ export default function App() {
         <Experience />
         <Contatti />
       </div>
+    <div className="bg-primary">
+      <Navbar />
+      <AboutMe></AboutMe>
+      <Skills></Skills>
+      <section
+        key={navbarElements[2].id}
+        id={navbarElements[2].id}
+        className="h-svh scroll-mt-14 p-4"
+      >
+          <h1 className="font-sans tracking-wide uppercase font-semibold">{navbarElements[2].title}</h1>
+      </section>
+      <section
+        key={navbarElements[3].id}
+        id={navbarElements[3].id}
+        className="h-svh scroll-mt-14 p-4"
+      >
+          <h1 className="font-sans tracking-wide uppercase font-semibold">{navbarElements[3].title}</h1>
+      </section>
+      <section
+        key={navbarElements[4].id}
+        id={navbarElements[4].id}
+        className="h-svh scroll-mt-14 p-4"
+      >
+          <h1 className="font-sans tracking-wide uppercase font-semibold">{navbarElements[4].title}</h1>
+      </section>
     </div>
   );
 }
