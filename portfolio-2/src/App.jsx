27c2c60cdf4e1a0345053d31b385b00fba6b1123
simone-2006 <simone.penza06@gmail.com>
2026-08-21 11:27:30 +0200

    .

--- portfolio-2/src/App.jsx
+++ portfolio-2/src/App.jsx
@@ -11,7 +11,6 @@ import FluidCursor from "./components/ui/FluidCursor";
 export default function App() {
   return (
     <div className="relative isolate min-h-svh bg-primary">
-      <FluidCursor />
       <div className="relative z-10">
         <Navbar />
         <AboutMe />
@@ -19,31 +18,6 @@ export default function App() {
         <Experience />
         <Contatti />
       </div>
-    <div className="bg-primary">
-      <Navbar />
-      <AboutMe></AboutMe>
-      <Skills></Skills>
-      <section
-        key={navbarElements[2].id}
-        id={navbarElements[2].id}
-        className="h-svh scroll-mt-14 p-4"
-      >
-          <h1 className="font-sans tracking-wide uppercase font-semibold">{navbarElements[2].title}</h1>
-      </section>
-      <section
-        key={navbarElements[3].id}
-        id={navbarElements[3].id}
-        className="h-svh scroll-mt-14 p-4"
-      >
-          <h1 className="font-sans tracking-wide uppercase font-semibold">{navbarElements[3].title}</h1>
-      </section>
-      <section
-        key={navbarElements[4].id}
-        id={navbarElements[4].id}
-        className="h-svh scroll-mt-14 p-4"
-      >
-          <h1 className="font-sans tracking-wide uppercase font-semibold">{navbarElements[4].title}</h1>
-      </section>
     </div>
   );
 }
